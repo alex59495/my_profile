8c1a2d2b2ea46c48c97eb98cfffdba95a75f498f
Alexis Lenoir <alexis59495@gmail.com>
2020-07-07 18:52:20 -0300

    Profile

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,10 +25,10 @@ require("channels")
 // External imports
 import "bootstrap";
 
-// Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
+// Internal imports
+import { snakeGame } from '../components/snake_game';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
-  // initSelect2();
+  snakeGame();
 });
