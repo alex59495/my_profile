57002c2570ccc64d0259436f4393a668ca93f210
Alexis Lenoir <alexis59495@gmail.com>
2020-09-11 20:31:25 -0300

    Delete forced ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   config.cache_classes = true
 
   # Force https in prod
-  config.force_ssl = true
+  config.force_ssl = false
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
