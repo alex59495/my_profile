1b136101b316bab0a85df002a09a1e61d341ec96
Alexis Lenoir <alexis.lenoir@sightcall.com>
2022-05-27 21:09:32 -0300

    Update profile

--- app/javascript/components/active_icon.js
+++ app/javascript/components/active_icon.js
@@ -7,38 +7,47 @@ const activeIcon = () => {
   const iconProjects = document.getElementById("icon-projects");
   const home = document.getElementById("home");
   const iconHome = document.getElementById("icon-home");
-  const about = document.getElementById("about-me");
+  const works = document.getElementById("works");
+  const iconWorks = document.getElementById("icon-works");
 
   const observerGame = new IntersectionObserver(function(entries) {
-    if(entries[0].isIntersecting === true) {
+    if(entries[0].isIntersecting) {
       iconGame.classList.add('active');
     } else {
       iconGame.classList.remove('active');
-    }}, { threshold: [0.35] });
+    }});
   observerGame.observe(game);
 
+  const observerWorks = new IntersectionObserver(function(entries) {
+    if(entries[0].isIntersecting) {
+      iconWorks.classList.add('active');
+    } else {
+      iconWorks.classList.remove('active');
+    }});
+  observerWorks.observe(works);
+
   const observerSkills = new IntersectionObserver(function(entries) {
-    if(entries[0].isIntersecting === true) {
+    if(entries[0].isIntersecting) {
       iconSkills.classList.add('active');
     } else {
       iconSkills.classList.remove('active');
-    }}, { threshold: [0.5] });
+    }});
   observerSkills.observe(skills);
 
   const observerProjects = new IntersectionObserver(function(entries) {
-    if(entries[0].isIntersecting === true) {
+    if(entries[0].isIntersecting) {
       iconProjects.classList.add('active');
     } else {
       iconProjects.classList.remove('active');
-    }}, { threshold: [0.5] });
+    }});
   observerProjects.observe(projects);
 
   const observerHome = new IntersectionObserver(function(entries) {
-    if(entries[0].isIntersecting === true) {
+    if(entries[0].isIntersecting) {
       iconHome.classList.add('active');
     } else {
       iconHome.classList.remove('active');
-    }}, { threshold: [0.2] });
+    }});
   observerHome.observe(home);
 }
 
--- app/models/experience.rb
+++ app/models/experience.rb
@@ -2,11 +2,13 @@ class Experience < ApplicationRecord
   EXPERIENCES = [{
     period: '.sightcall-period',
     description: '.sightcall-description',
-    image: 'Sightcall.png'
+    image: 'Sightcall.png',
+    url: 'https://sightcall.com/'
   },
   {
     period: '.freelance-period',
     description: '.freelance-description',
-    image: 'freelance.png'
+    image: 'freelance.png',
+    url: 'https://github.com/alex59495'
   }]
 end
